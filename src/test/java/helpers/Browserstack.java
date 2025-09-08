db61709f14f87ee78f5ccdebcf5d5d2fd7fbf2c2
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-09-08 20:34:04 +0300

    added class Browserstack

--- src/test/java/helpers/Browserstack.java
+++ src/test/java/helpers/Browserstack.java
@@ -1,6 +1,21 @@
 package helpers;
 
+import static io.restassured.RestAssured.given;
+
 public class Browserstack {
     //curl -u "bsuser_1dr2kM:S52zPpUxckvmDS3C7USu" -X GET "https://api.browserstack.com/app-automate/sessions/<session-id>.json"
+    // automation_session.video_url
 
+    public static String videoUrl(String sessionId) {
+        String url = String.format("https://api.browserstack.com/app-automate/sessions/%s.json", sessionId);
+
+        return given()
+                .auth().basic("bsuser_1dr2kM", "S52zPpUxckvmDS3C7USu")
+                .get(url)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(200)
+                .extract().path("automation_session.video_url");
+    }
 }
