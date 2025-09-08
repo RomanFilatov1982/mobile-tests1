dcfe69f47bf14811e07043da4b6599f11177e3e1
Roman Filatov <110454660+RomanFilatov1982@users.noreply.github.com>
2025-09-08 16:01:21 +0300

    first commit

---
+++ src/test/java/tests/SearchTests.java
@@ -0,0 +1,36 @@
+package tests;
+
+import Drivers.BrowserstackDriver;
+import com.codeborne.selenide.CollectionCondition;
+import com.codeborne.selenide.Configuration;
+import io.appium.java_client.AppiumBy;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+import java.util.List;
+
+import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$$;
+import static io.appium.java_client.AppiumBy.*;
+import static io.qameta.allure.Allure.step;
+
+public class SearchTests extends TestBase {
+    @Test
+    void successfulSearchTest() {
+        step("Type search", () -> {
+            $(accessibilityId("Search Wikipedia")).click();
+            $(id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Appium");
+        });
+        step("Verify content found", () -> {
+            $$(className("android.widget.TextView")).shouldHave(sizeGreaterThan(0));
+        });
+    }
+}
